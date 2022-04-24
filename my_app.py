45d4b65dfb8287d816de1c14d0e66a38dd59ba3a
Algo04 <Algo04@mail.ru>
2022-04-24 12:13:36 +0500

    timer.v1

--- my_app.py
+++ my_app.py
@@ -1,6 +1,7 @@
 # напиши здесь код основного приложения и первого эк
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import Qt, QTimer, QTime 
 
 from instr import *
 from second_win import *
@@ -20,11 +21,14 @@ class MainWin(QWidget):
     def initUI(self):
         self.hello_text = QLabel(txt_hello)
         self.instruction = QLabel(txt_instruction)
-        self.button = QPushButton(txt_next)
+        self.button = QPushButton(txt_next)        
     def connects(self):
         self.btn_next.clicked.connect(self.next_click)
     def next_click(self):
         self.hide()
         self.tw = TestWin(QWidget)
 
-
+app = QApplication([])
+win = MainWin()
+win.show()
+app.exec_()
--- second_win.py
+++ second_win.py
@@ -1,7 +1,27 @@
 # напиши здесь код для второго экрана приложения
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
 
 class TestWin(QWidget):
     def __init__(self):
         pass
+
+    def timer_test(self):
+        self.timer = QTime(0, 1, 0)
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1000)
+
+
+    def timer1Event(self):
+        global time 
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString('hh:mm:ss'))
+        self.text_timer.setFont(QFont('Times', 36, QFont.bold))
+        self.text_timer.setStyleSheet('color: rgb(0, 0, 0)')
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer.stop()
+
+    def connects(self):
+        self.btn_test1.clicked.connect(self.timer_test)
